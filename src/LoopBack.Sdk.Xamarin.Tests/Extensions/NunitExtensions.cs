05f47c6cfb089a9fb65e74b3a4b5f7b2fc8e5ac4
Ziya SARIKAYA <sarikayaziya@gmail.com>
2014-10-21 21:19:46 +0300

    * NunitExtensions.cs: Add Nunit extensions
    * LoopBack.Sdk.Xamarin.Tests.csproj:

---
+++ src/LoopBack.Sdk.Xamarin.Tests/Extensions/NunitExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace LoopBack.Sdk.Xamarin.Tests
+{
+	public static class NunitExtensions
+	{
+		public static async Task ThrowsAsync<TException>(Func<Task> func) where TException : Exception
+		{
+			var expected = typeof (TException);
+			Type actual = null;
+
+			try
+			{
+				await func();
+			}
+			catch (TException exception)
+			{
+				actual = exception.GetType();
+			}
+
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
