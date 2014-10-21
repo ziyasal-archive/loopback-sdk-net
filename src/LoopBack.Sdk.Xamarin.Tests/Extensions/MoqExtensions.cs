c67c39e343c2bba28a23c0877a204ef710609f05
Ziya SARIKAYA <sarikayaziya@gmail.com>
2014-10-21 21:29:40 +0300

    * MoqExtensions.cs: Add Moq extensions
    * LoopBack.Sdk.Xamarin.Tests.csproj:

---
+++ src/LoopBack.Sdk.Xamarin.Tests/Extensions/MoqExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Moq.Language.Flow;
+using System.Threading.Tasks;
+using Moq.Language;
+
+namespace LoopBack.Sdk.Xamarin.Tests
+{
+	public static class MoqExtensions
+	{
+		public static IReturnsResult<TMock> ReturnsAsync<TMock, TResult>(
+			this IReturns<TMock, Task<TResult>> setup, TResult value)
+			where TMock : class
+		{
+			return setup.Returns(Task.FromResult(value));
+		}
+	}
+}
