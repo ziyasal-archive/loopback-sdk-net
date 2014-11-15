db4a8438f800e36fd9792bce11e1e30bb9864a62
Christopher Svanefalk <christopher.svanefalk@gmail.com>
2014-11-15 19:16:59 +0100

    Fix package setup to solve broken BCL refs

--- src/LoopBack.Sdk.Xamarin.Tests/Remooting/DummyContextImpl.cs
+++ src/LoopBack.Sdk.Xamarin.Tests/Remooting/DummyContextImpl.cs
@@ -4,5 +4,18 @@ namespace LoopBack.Sdk.Xamarin.Tests.Remooting
 {
     public class DummyContextImpl : IContext
     {
+		string _UserAgent;
+		public string UserAgent {
+			get {
+				return _UserAgent;
+			}
+			set {
+				_UserAgent = value;
+			}
+		}
+
+		public DummyContextImpl(string UserAgent) {
+			_UserAgent = UserAgent;
+		}
     }
 }
